ebc44b2217d7d53380591b180b45526e875f2ef4
Rafael PacMe <rafy.pachecomendez@gmail.com>
2015-12-11 19:09:21 -0300

    revert: surfaceflinger: allow surface format selection for NO_RGBX_8888 devices

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -2127,7 +2127,7 @@ status_t SurfaceFlinger::createNormalLayer(const sp<Client>& client,
         format = PIXEL_FORMAT_RGBA_8888;
         break;
     case PIXEL_FORMAT_OPAQUE:
-        format = mPrefer16bpp ? PIXEL_FORMAT_RGB_565 : PIXEL_FORMAT_RGBX_8888;
+        format = PIXEL_FORMAT_RGBX_8888;
         break;
     }
 
